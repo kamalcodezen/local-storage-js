138893960cb9f7d7ae42031a24ed854f1536c361
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-10 00:49:32 +0530

    display show

--- javaScript/script.js
+++ javaScript/script.js
@@ -23,6 +23,47 @@ const getObjectItem = () => {
 
     const customerJson = localStorage.getItem("customer")
     const customer = JSON.parse(customerJson)
-    console.log( customer.name)
+    console.log(customer.name)
+
+}
+
+
+const mainAllProduct = () => {
+    const productEl = document.getElementById("product");
+    const quantityEl = document.getElementById("quantity");
+
+    const product = productEl.value;
+    const quantity = quantityEl.value;
+
+    displayProduct(product, quantity);
+    addToProduct(product, quantity);
+
+    productEl.value = "";
+    quantityEl.value = "";
+}
+
+
+
+const getCart = () => {
+    const cart = {};
+    return cart;
+}
+
+const addToProduct = (product, quantity) => {
+    let cart = getCart();
+    cart[product] = quantity;
+    console.log("cart", cart)
+
+}
+
+
+const displayProduct = (product, quantity) => {
+
+    const li = document.createElement("li")
+    li.innerText = `${product} : ${quantity}`
+
+    // get div and append
+    const productList = document.getElementById("product-list");
+    productList.append(li)
 
 }
