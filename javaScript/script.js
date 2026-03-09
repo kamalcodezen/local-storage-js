bba3253d9dc958bf067095029510da18f8098e66
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-09 13:43:31 +0530

    localStorage setItem and getItem practice

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,14 @@
-console.log("local Storage")
+
+
+const addNumberToLs = () => {
+    const number = Math.ceil(Math.random() * 100);
+    localStorage.setItem("number", number);
+    console.log(number)
+
+}
+const getNumberToLs= () => {
+    const number = localStorage.getItem("number");
+    console.log("number", number)
+
+}
+// getItem()
